264a405c6c82411f96af7f45b8e66a165c6dea22
sadurtinova <sadurtinova@speechpro.com>
2019-02-05 17:22:56 +0300

    Update for version 1.1

---
+++ diarization/run.js
@@ -0,0 +1,30 @@
+var DiarizationModule = require('diarization');
+var btoa = require('btoa');
+const util = require('util');
+var session = new DiarizationModule.SessionApi();
+var auth = new DiarizationModule.AuthRequestDto("user", 261, "password");
+var diarizationCallback = function(error, data, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('API called successfully');
+        console.log(util.inspect(data.speakers, false, null, true));
+    }
+};
+var startSessionCallback = function(error, data, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('API called successfully. Returned data: ' + data.session_id);
+        var sessionId = data.session_id;
+        var diarizationApi = new DiarizationModule.DiarizationApi();
+        var fs = require('fs'),
+            path = require('path'),
+            filePath = path.join('test.wav');
+        var contents = fs.readFileSync(filePath, 'utf8');
+        var base64 = btoa(contents);
+        var diarizationAudio = new DiarizationModule.AudioDto(base64);
+        diarizationApi.diarization(sessionId, diarizationAudio, diarizationCallback);
+    }
+};
+session.login(auth, startSessionCallback);
